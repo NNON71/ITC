fc915196a663f5775b2d3639fc8aafc2e8027043
NNON <wasitpolmaliwan@gmail.com>
2022-09-07 23:38:19 +0700

    add

--- assignment/3/Assignment3/Assignment3.ino
+++ assignment/3/Assignment3/Assignment3.ino
@@ -1,69 +1,105 @@
 int number; // number to display
-int bitPattern[10] = {B01000001, B01111011, B11000010, B01010010, B01111000, B01010100, B01000100, B01110011, B01000000, B01010000};
-int bitans[3] = {B01000001,B01000101,B11001101};
+int bitPattern[9] = {B11101101, B01000011, B01001001, B00101101, B00011001, B00010001, B11001101, B00000001, B00001001};
+int bitans[3] = {B10000001, B10010001, B10110011};
 #define buttonA 13
-#define buttonB 2
+#define buttonB 12
 const byte numPins = 8;
 const int segmentPins[8] = {11, 10 , 9, 8, 7, 6, 5, 4};
 int i = 0;
-unsigned int pastA=0;
-int bob;
+unsigned int pastA = 0;
+unsigned int pastB = 0;
+int randomNo ;
+boolean isBitSet;
 void setup () {
   Serial.begin(9600);
   for (int i = 0; i < numPins; i++)
     pinMode(segmentPins[i], OUTPUT);
   pinMode(buttonA, INPUT);
   pinMode(buttonB, INPUT);
-  bob = random(1,9);
+  for (int segment = 0; segment < 8; segment++)
+  {
+    isBitSet = bitRead(bitPattern[0], segment);
+    digitalWrite(segmentPins[segment], isBitSet);
+  }
+  randomSeed(analogRead(A0));
+  randomNo  = random(1, 10);
+  Serial.println(String("random = ") + randomNo );
+
 }
-void loop() {
-  //Serial.println(digitalRead(buttonB));
-  Serial.println(String("random = ")+bob);
-  if (digitalRead(buttonA)==0) {
-    //Serial.println(i);
-    if (millis() - pastA >= 400) {
+
+void switchA() {
+  //Serial.println(digitalRead(buttonA));
+  if (digitalRead(buttonA) == 1)
+  {
+    Serial.println(i);
+    if (millis() - pastA >= 600)
+    {
       pastA = millis();
-      i = i + 1;
-      boolean isBitSet;
-      for (int segment = 0; segment < 8; segment++) 
-      {
+      i++;
+      if (i > 8) i = 0;
+      /*
+        for (int segment = 0; segment < 8; segment++)
+        {
         isBitSet = bitRead(bitPattern[i], segment);
         digitalWrite(segmentPins[segment], isBitSet);
-      }
-      if(i>=9) {i=0;}
+        }
+      */
     }
   }
-  
-//  if (digitalRead(buttonB)==1){
-//    if(i==bob)
-//    {
-//      
-//      boolean isBitSet;
-//      for (int segment = 0; segment < 8; segment++) 
-//      {
-//        isBitSet = bitRead(bitans[0], segment);
-//        digitalWrite(segmentPins[segment], isBitSet);
-//      }
-//    }
-//    
-//    if(i > bob)
-//    {
-//      boolean isBitSet;
-//      for (int segment = 0; segment < 8; segment++) 
-//      {
-//        isBitSet = bitRead(bitans[1], segment);
-//        digitalWrite(segmentPins[segment], isBitSet);
-//      }
-//    }
-//    if(i < bob)
-//    {
-//      boolean isBitSet;
-//      for (int segment = 0; segment < 8; segment++) 
-//      {
-//        isBitSet = bitRead(bitans[2], segment);
-//        digitalWrite(segmentPins[segment], isBitSet);
-//      }
-//    }
-//  }
 }
- 
+
+void switchB() {
+  if (digitalRead(buttonB) == 1)
+  {
+    if (millis() - pastB >= 400)
+    {
+      pastB = millis();
+      if (i + 1 == randomNo )
+      {
+
+        for (int segment = 0; segment < 8; segment++)
+        {
+          isBitSet = bitRead(bitans[0], segment);
+          digitalWrite(segmentPins[segment], isBitSet);
+        }
+        delay(1000);
+        randomNo  = random(1, 10);
+        i = 0;
+        Serial.println(String("random = ") + randomNo );
+      }
+
+      else if (i + 1 > randomNo )
+      {
+
+        for (int segment = 0; segment < 8; segment++)
+        {
+          isBitSet = bitRead(bitans[1], segment);
+          digitalWrite(segmentPins[segment], isBitSet);
+        }
+        delay(1000);
+      }
+      else if (i + 1 < randomNo )
+      {
+
+        for (int segment = 0; segment < 8; segment++)
+        {
+          isBitSet = bitRead(bitans[2], segment);
+          digitalWrite(segmentPins[segment], isBitSet);
+        }
+        delay(1000);
+      }
+    }
+  }
+}
+
+void loop() {
+  switchA();
+  //Serial.println(i);
+  switchB();
+  delay(100);
+  for (int segment = 0; segment < 8; segment++)
+  {
+    isBitSet = bitRead(bitPattern[i], segment);
+    digitalWrite(segmentPins[segment], isBitSet);
+  }
+}
